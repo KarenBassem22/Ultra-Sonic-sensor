d844f8a00da4e603d5a18bf198752fe666150b3e
KarenBassem22 <85080830+KarenBassem22@users.noreply.github.com>
2021-10-17 01:42:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,19 +11,18 @@ int main(void){
 	SREG|=(1<<7);
 	ULTRASONIC_init();
 	LCD_displayString("Distance=   cm");
-	//_delay_ms(1000);
-	//LCD_sendCommand(CLEAR_SCREEN);
-	//LCD_moveCursor(2,10);
-	//LCD_displayCharacter('k');
 	float distance=0;
 	while(1){
 
-		Ultrasonic_Trigger();
-		if(g_edgeCount==2){
 		distance=Ultrasonic_readDistance();
 		LCD_moveCursor(0,9);
 		LCD_integerToString(distance);
-		g_edgeCount=0;
+		if(distance<100){
+			LCD_displayCharacter(' ');
+		}
+		if(distance<10){
+			LCD_displayCharacter(' ');
 		}
 	}
 }
+
