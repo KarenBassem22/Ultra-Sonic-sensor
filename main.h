2210664a53d855cdc164d1f617490a8cff48007c
KarenBassem22 <85080830+KarenBassem22@users.noreply.github.com>
2021-10-17 00:12:22 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * File name: fourth_mini_project.h
+ * Description: header file for measuring distance using ultrasonic sensor
+ * Date: Oct 15, 2021
+ * Author: Karen Bassem
+ ******************************************************************************/
+
+#ifndef FOURTH_MINI_PROJECT_H_
+#define FOURTH_MINI_PROJECT_H_
+
+#include "avr\io.h"
+#include "std_types.h"
+#include "common_macros.h"
+#include "gpio.h"
+#include "LCD_driver.h"
+#include "ICU.h"
+#include "ULTRASONIC_sensor.h"
+
+#endif /* FOURTH_MINI_PROJECT_H_ */
